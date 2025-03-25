22541d16e2190f58481121cb75177bb534971c03
ingaSiu <ingasiud@gmail.com>
2025-03-25 20:21:43 +0200

    add more types for forecast data

--- src/types/forecast.ts
+++ src/types/forecast.ts
@@ -17,3 +17,29 @@ export type Forecast = {
     windSpeed: number;
   }[];
 };
+
+export type ForecastTimestamps = {
+  airTemperature: number;
+  cloudCover: number;
+  conditionCode: string;
+  feelsLikeTemperature: number;
+  forecastTimeUtc: string;
+  relativeHumidity: number;
+  seaLevelPressure: number;
+  totalPrecipitation: number;
+  windDirection: number;
+  windGust: number;
+  windSpeed: number;
+}[];
+
+export type DailyForecast = {
+  date: string;
+  day: {
+    maxTemperature: number;
+    conditionCode: string;
+  };
+  night: {
+    minTemperature: number;
+    conditionCode: string;
+  };
+};
